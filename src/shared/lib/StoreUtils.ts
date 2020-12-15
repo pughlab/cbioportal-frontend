9c3d9d0b5a1e2f1ae7b838e18e41aee084ad009d
Prasanna Kumar Jagannathan <37613906+jagnathan@users.noreply.github.com>
2020-12-15 11:54:11 -0500

    Update StoreUtils.ts
    Fixed parameter

--- src/shared/lib/StoreUtils.ts
+++ src/shared/lib/StoreUtils.ts
@@ -1083,7 +1083,7 @@ export async function fetchPharmacoDbCnaView(oncotreecode:MobxPromise<string>,
             }
         });
     
-        let pharmacoDBViewList: IPharmacoDBViewList = (await getPharmacoDBCnaView(oncoTreeCode,querySymbols));
+        let pharmacoDBViewList: IPharmacoDBViewList = (await getPharmacoDBCnaView(otc,querySymbols));
     
         return pharmacoDBViewList;
     } else {

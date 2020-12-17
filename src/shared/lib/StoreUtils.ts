7c6ecb43b4520898731398977f1eb9f56f848824
Prasanna Kumar Jagannathan <37613906+jagnathan@users.noreply.github.com>
2020-12-17 14:12:57 -0500

    Updates to pharmacoDB integration
    Changes to API url, Utils and types and global settings

--- src/shared/lib/StoreUtils.ts
+++ src/shared/lib/StoreUtils.ts
@@ -1050,12 +1050,12 @@ export async function fetchDiscreteCNAData(
 PharmacoDB CNA View
 */ 
 
-export async function fetchPharmacoDbCnaView(oncotreecode:MobxPromise<string>,
+export async function fetchPharmacoDbCnaView(oncotreecode:string,
                                              discreteCNAData:MobxPromise<DiscreteCopyNumberData[]>)
 {
     let otc:string = '';
     
-    otc = oncotreecode.result || '';
+    otc = oncotreecode;
 
     if (discreteCNAData.result && discreteCNAData.result.length > 0) {
          

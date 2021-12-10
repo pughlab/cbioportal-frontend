07271c4454c117db5679af9602d7d7c847abf5b7
Mickey Ng <mickey.ng@rmp.uhn.ca>
2021-12-10 14:36:37 -0500

    update API class to match new matchminer API library(WIP)

--- src/shared/api/MatchMinerAPI.ts
+++ src/shared/api/MatchMinerAPI.ts
@@ -7,48 +7,90 @@ import { buildCBioPortalAPIUrl } from './urls';
  */
 // It cannot be set globally since it will cause test error: undefined of 'replace'.
 // const cbioportalUrl = buildCBioPortalAPIUrl('api/matchminer/api');
+// http://localhost:8081/api/matches/PTEN/K183Rfs*16?sample=OCT-01-0082-555Panel
 export async function fetchTrialMatchesUsingPOST(
     query: object
 ): Promise<Array<ITrialMatch>> {
-    const cbioportalUrl = buildCBioPortalAPIUrl('api/matchminer/api');
+    const cbioportalUrl = buildCBioPortalAPIUrl('api');
+    console.log('query: ' + JSON.stringify(query));
     return request
-        .post(cbioportalUrl + '/post_trial_match')
+        // .post(cbioportalUrl + '/post_trial_match')
+        .get(cbioportalUrl + '/trial_match/' + query.mrn)
+        // .get(cbioportalUrl + '/trial_match')
         .set('Content-Type', 'application/json')
         .send(query)
         .then(res => {
-            const response = JSON.parse(res.text);
-            return response.map((record: any) => ({
-                id: record.nct_id + '+' + record.protocol_no,
-                nctId: record.nct_id,
-                protocolNo: record.protocol_no,
-                gender: record.gender ? record.gender : '',
-                matchType: record.match_type ? record.match_type : '',
-                armDescription: record.arm_description
-                    ? record.arm_description
-                    : '',
-                armType: record.arm_type ? record.arm_type : '',
-                sampleId: record.sample_id,
-                mrn: record.mrn,
-                vitalStatus: record.vital_status ? record.vital_status : '',
-                genomicAlteration: record.genomic_alteration
-                    ? record.genomic_alteration
-                    : '',
-                trueHugoSymbol: record.true_hugo_symbol
-                    ? record.true_hugo_symbol
-                    : '',
-                trueProteinChange: record.true_protein_change
-                    ? record.true_protein_change
-                    : '',
-                oncotreePrimaryDiagnosisName: record.oncotreePrimaryDiagnosisName
-                    ? record.oncotreePrimaryDiagnosisName
-                    : '',
-                trialAgeNumerical: record.trial_age_numerical
-                    ? record.trial_age_numerical
-                    : '',
-                trialOncotreePrimaryDiagnosis: record.trial_oncotree_primary_diagnosis
-                    ? record.trial_oncotree_primary_diagnosis
-                    : '',
-            }));
+            const myResponse = JSON.parse(res.text);
+            const response = myResponse._items;
+            let ret:Array<ITrialMatch> = [];
+            response.forEach((record: any) => {
+                let curRecord: ITrialMatch = {
+                    id: record.nct_id + '+' + record.protocol_no,
+                    nctId: record.nct_id,
+                    protocolNo: record.protocol_no,
+                    gender: record.gender ? record.gender : '',
+                    matchType: record.match_type ? record.match_type : '',
+                    armDescription: record.arm_description
+                        ? record.arm_description
+                        : '',
+                    armType: record.arm_type ? record.arm_type : '',
+                    sampleId: record.sample_id,
+                    mrn: record.mrn,
+                    vitalStatus: record.vital_status ? record.vital_status : '',
+                    genomicAlteration: record.genomic_alteration
+                        ? record.genomic_alteration
+                        : '',
+                    trueHugoSymbol: record.true_hugo_symbol
+                        ? record.true_hugo_symbol
+                        : '',
+                    trueProteinChange: record.true_protein_change
+                        ? record.true_protein_change
+                        : '',
+                    oncotreePrimaryDiagnosisName: record.oncotree_primary_diagnosis_name
+                        ? record.oncotree_primary_diagnosis_name
+                        : '',
+                    trialAgeNumerical: record.trial_age_numerical
+                        ? record.trial_age_numerical
+                        : '',
+                    trialOncotreePrimaryDiagnosis: record.trial_oncotree_primary_diagnosis
+                        ? record.trial_oncotree_primary_diagnosis
+                        : '',
+                };
+                ret.push(curRecord);
+            });
+            // let ret = response.map((record: any) => ({
+            //     id: record.nctID + '+' + record.protocol_no,
+            //     nctId: record.nct_id,
+            //     protocolNo: record.protocol_no,
+            //     gender: record.gender ? record.gender : '',
+            //     matchType: record.match_type ? record.match_type : '',
+            //     armDescription: record.arm_description
+            //         ? record.arm_description
+            //         : '',
+            //     armType: record.arm_type ? record.arm_type : '',
+            //     sampleId: record.sample_id,
+            //     mrn: record.mrn,
+            //     vitalStatus: record.vital_status ? record.vital_status : '',
+            //     genomicAlteration: record.genomic_alteration
+            //         ? record.genomic_alteration
+            //         : '',
+            //     trueHugoSymbol: record.true_hugo_symbol
+            //         ? record.true_hugo_symbol
+            //         : '',
+            //     trueProteinChange: record.true_protein_change
+            //         ? record.true_protein_change
+            //         : '',
+            //     oncotreePrimaryDiagnosisName: record.oncotreePrimaryDiagnosisName
+            //         ? record.oncotreePrimaryDiagnosisName
+            //         : '',
+            //     trialAgeNumerical: record.trial_age_numerical
+            //         ? record.trial_age_numerical
+            //         : '',
+            //     trialOncotreePrimaryDiagnosis: record.trial_oncotree_primary_diagnosis
+            //         ? record.trial_oncotree_primary_diagnosis
+            //         : '',
+            // }));
+            return ret;
         });
 }
 
@@ -56,7 +98,7 @@ export async function fetchTrialsByTypeAndId(
     type: string,
     id: string
 ): Promise<ITrial> {
-    const cbioportalUrl = buildCBioPortalAPIUrl('api/matchminer/api');
+    const cbioportalUrl = buildCBioPortalAPIUrl('api');
     return request.get(cbioportalUrl + '/' + type + '/' + id).then(res => {
         const response = JSON.parse(res.text);
         return {
@@ -75,7 +117,7 @@ export async function fetchTrialsByTypeAndId(
 export async function fetchTrialsUsingPost(
     query: object
 ): Promise<Array<ITrial>> {
-    const cbioportalUrl = buildCBioPortalAPIUrl('api/matchminer/api');
+    const cbioportalUrl = buildCBioPortalAPIUrl('api');
     return request
         .post(cbioportalUrl + '/post_trial')
         .set('Content-Type', 'application/json')
@@ -95,22 +137,39 @@ export async function fetchTrialsUsingPost(
 }
 
 export async function fetchTrialsById(query: object): Promise<Array<ITrial>> {
-    const cbioportalUrl = buildCBioPortalAPIUrl('api/matchminer/api');
+    const cbioportalUrl = buildCBioPortalAPIUrl('api');
     return request
-        .post(cbioportalUrl + '/trials')
+        .get(cbioportalUrl + '/trial')
+        // .get(cbioportalUrl + '/trial')
         .set('Content-Type', 'application/json')
         .send(query)
         .then(res => {
-            const response = JSON.parse(res.text);
-            return response.map((record: any) => ({
-                id: record.nct_id + '+' + record.protocol_no,
-                nctId: record.nct_id,
-                protocolNo: record.protocol_no,
-                principalInvestigator: record.principal_investigator,
-                phase: record.phase,
-                shortTitle: record.short_title,
-                status: record.status,
-                treatmentList: record.treatment_list,
-            }));
+            const myResponse = JSON.parse(res.text);
+            const response = myResponse._items;
+            // return response.map((record: any) => ({
+            //     id: record.nct_id + '+' + record.protocol_no,
+            //     nctId: record.nct_id,
+            //     protocolNo: record.protocol_no,
+            //     principalInvestigator: record.principal_investigator,
+            //     phase: record.phase,
+            //     shortTitle: record.short_title,
+            //     status: record.status,
+            //     treatmentList: record.treatment_list,
+            // }));
+            let ret:Array<ITrial> = [];
+            response.forEach((record: any) => {
+               let curRecord: ITrial = {
+                   id: record.nct_id + '+' + record.protocol_no,
+                       nctId: record.nct_id,
+                       protocolNo: record.protocol_no,
+                       principalInvestigator: record.principal_investigator,
+                       phase: record.phase,
+                       shortTitle: record.short_title,
+                       status: record.status,
+                       treatmentList: record.treatment_list,
+               }
+               ret.push(curRecord);
+            });
+            return ret;
         });
 }

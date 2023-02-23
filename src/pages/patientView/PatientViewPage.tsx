c85558e8574bf7c603d6dff50136f2fff7ec2156
Prasanna Kumar Jagannathan <37613906+jagnathan@users.noreply.github.com>
2023-02-23 10:56:06 -0500

    support for eslidemanager tab

--- src/pages/patientView/PatientViewPage.tsx
+++ src/pages/patientView/PatientViewPage.tsx
@@ -26,6 +26,7 @@ import {
     getPatientViewUrl,
     getWholeSlideViewerUrl,
 } from '../../shared/api/urls';
+import { getPathologySlideUrl } from '../../shared/api/urls';
 import { PageLayout } from '../../shared/components/PageLayout/PageLayout';
 import Helmet from 'react-helmet';
 import { getServerConfig } from '../../config/config';
@@ -251,6 +252,15 @@ export class PatientViewPageInner extends React.Component<
         );
     }
 
+    @computed get showPathologySlideViewerTab() {
+        return (
+            this.patientViewPageStore.clinicalDataPatient.isComplete &&
+            _.some(this.patientViewPageStore.clinicalDataPatient.result, s => {
+                return s.clinicalAttributeId === 'SLIDE_ID';
+            })
+        );
+    }
+
     @action.bound
     onCnaTableColumnVisibilityToggled(
         columnId: string,
@@ -335,6 +345,21 @@ export class PatientViewPageInner extends React.Component<
                     //but if request fails, we will return undefined.
                     return undefined;
                 }
+            }
+            return undefined;
+        },
+    });
+
+    pathologySlideViewerUrl = remoteData<string | undefined>({
+        await: () => [this.patientViewPageStore.clinicalDataPatient],
+        invoke: async () => {
+            if (
+                !_.isEmpty(this.patientViewPageStore.clinicalDataPatient.result)
+            ) {
+                const ids = this.patientViewPageStore.clinicalDataPatient.result.find(
+                    c => c.clinicalAttributeId === 'SLIDE_ID'
+                )?.value;
+                return ids ? getPathologySlideUrl(ids) : '';
             }
             return undefined;
         },
